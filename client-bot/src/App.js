cb105dcf2ae239ce2bd0d927770d51326a050acd
kalys <61675998+kalyskhairy@users.noreply.github.com>
2020-06-26 12:01:12 +0700

    initial commit

---
+++ client-bot/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FirstPage from './VIEWS/firstPage'
+
+function App() {
+  return (
+    <div className="App">
+     <FirstPage/>
+    </div>
+  );
+}
+
+export default App;
